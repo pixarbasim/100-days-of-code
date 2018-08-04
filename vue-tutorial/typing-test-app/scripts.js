e91a7fc84e8d38f4e76b3d7df37b2e922c488d1e
basim ahmad <pixar.basim@gmail.com>
2018-08-05 02:58:16 +0530

    Binded input text and user input to vue

--- vue-tutorial/typing-test-app/scripts.js
+++ vue-tutorial/typing-test-app/scripts.js
@@ -1,6 +1,15 @@
 var root = new Vue({
   el: '#root',
   data: {
-
+    textToType: 'The prosecutor said if the mathematical mistakes are set right, the quantum of disproportionate assets owned by Jayalalithaa would be in excess of 76% of her known sources of income and not 8.12 % as arrived by the court.',
+    userInput : ''
   },
+  watch : {
+      userInput : 'onUserInputChange'
+  },
+  methods : {
+      onUserInputChange : function () {
+          console.log(this.userInput)
+      }
+  }
 })
